9ffb56a0d2d20232182b0d1fb07fe508ce9add37
Ahmed Taha <devtaha@Ahmeds-MacBook-Pro.local>
2022-07-23 23:42:51 +0200

    finish section 3 v2

--- index.php
+++ index.php
@@ -3,5 +3,4 @@
 require_once './Services/Maqe.php';
 require_once './Services/WalkingCommand.php';
 
-$maqe = new Maqe(new WalkingCommand($_SERVER['argv']));
-$maqe->run();
+(new Maqe(new WalkingCommand($_SERVER['argv'])))->run();
